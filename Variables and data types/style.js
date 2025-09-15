8888eac04efdac4d19a9d74537d10797acb60f98
gagan-k454545 <sincostan3030@gmail.com>
2025-09-15 21:23:05 +0530

    vARIABLE AND DATATYPE

---
+++ Variables and data types/style.js
@@ -0,0 +1,40 @@
+//*************************Variables in js********************
+//Variables are containers for data
+
+
+
+
+//*****************************Variables Rules**********************
+// >variables name are case sensitvie 'a' and 'A' are different 
+// >Only letters digits , underscore (-) and $ is allowde (not even spage )
+// >ONLYE A LETTER UNDERSCORE(_) OR $ SHOULD BE 1ST character
+// >Reservers word cannoth be vaiable name
+
+
+let FullName='Gagan';
+let LastName='k';
+console.log(FullName,LastName);  //Gagan k
+console.log(FullName+LastName); //Gagank
+
+
+//>fullName --> camel case  this should be used in js
+//>fullname --> xxx
+//full_name  -->snakecase
+//full-name -->kababcase
+//FullName ---> pascal case 
+
+isFOLLOW =true //camelcase
+totalPrise =null//camelcase
+
+
+//*****************let const car*************************** */\
+
+//>var VARIABLE CAN BE REDECLARED AND UPDATED A GLOBAL SCOPE VARIABLE 
+//>let variable cannot be redeclared but can be updated a block scope varibale
+//>const variable cannot be redeclared or updated a block scope variable
+
+
+
+let name ='GAGAN K';
+console.log(name); //GAGAN K
+
